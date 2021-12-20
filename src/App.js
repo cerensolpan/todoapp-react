2e2ae892d78e9d0924accf3f208d1f8d7666d449
cerensolpan <cerensolpan@gmail.com>
2021-12-21 01:33:42 +0300

    Add status to know completed list item

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   const addTodo = () => {
     let arr = todoList
-    arr.push({ index: index, todo: todoInput });
+    arr.push({ index: index, todo: todoInput, status: false });
     setIndex(index + 1);
     setTodoList(arr);
     setTodoInput("");
@@ -17,22 +17,31 @@ const App = () => {
     let deletedArr = todoList.filter((todo) => todo.index !== index );
     setTodoList(deletedArr);
   }
+
+  const completedTodo = (index) => {
+    let arr=todoList;
+    arr.map((todo)=> {
+      if(todo.index===index) todo.status=!todo.status
+    });
+    setTodoList([...arr]);
+  }
  
   return (
     <div className="flex flex-col bg-blue-100 justify-center items-center w-full min-h-screen">
-      <div className='w-full max-w-xs'>
-        <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4  flex flex-col items-end '>
+      <div className='w-full max-w-xs bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col '>
+        <h1 className='text-center text-lg font-semibold mb-2'>Todo List</h1>
+        <form className='flex flex-col items-end'>
           <input
             className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
             value={todoInput}
             onChange={(e) => { setTodoInput(e.target.value) }} /><hr />
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2' type='button' onClick={addTodo} >Add</button>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 mb-4' type='button' onClick={addTodo} >Add</button>
         </form>
+        <div>
+          {todoList.map(todo => { return <Todo key={todo.index} todo={todo} deleteTodo={deleteTodoCallback} completedTodo={completedTodo}/> })}
+        </div>
       </div>
-      <hr />
-      <div className=' flex flex-col'>
-        {todoList.map(todo => { return <Todo key={todo.index} todo={todo} deleteTodo={deleteTodoCallback}/> })}
-      </div>
+
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
-const Todo = ({ todo, deleteTodo }) => {
+const Todo = ({ todo, deleteTodo, completedTodo }) => {
     return (
-        <div className='my-1'>
-            <span>{todo.todo}</span>
-            <button 
-                className='bg-red-200 hover:bg-red-400 text-white font-bold py-2 px-3 rounded mx-2'
-                onClick={()=>deleteTodo(todo.index)}
-            ><FontAwesomeIcon icon={faTrash} /></button>
+        <div className='my-1 flex items-center justify-between'>
+            <span className={todo.status?'line-through': ''}>{todo.todo}</span>
+            <div>
+                <button 
+                    className='bg-teal-200 hover:bg-teal-400 text-white font-bold py-1 px-2 rounded-full mr-2'
+                    onClick={()=>completedTodo(todo.index)}
+                ><FontAwesomeIcon icon={faCheckCircle} /></button>
+                <button 
+                    className='bg-rose-200 hover:bg-rose-400 text-white font-bold py-1 px-2 rounded-full'
+                    onClick={()=>deleteTodo(todo.index)}
+                ><FontAwesomeIcon icon={faTrash} /></button>
+            </div>
         </div>
     )
 }
